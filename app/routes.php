118017b63750869b3b6f21c5365b873795ba056a
tahmina8765 <tahmina8765@yahoo.com>
2015-01-12 15:13:16 +0600

    init project

--- app/routes.php
+++ app/routes.php
@@ -1,22 +1,29 @@
 <?php
 
 /*
-|--------------------------------------------------------------------------
-| Application Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register all of the routes for an application.
-| It's a breeze. Simply tell Laravel the URIs it should respond to
-| and give it the Closure to execute when that URI is requested.
-|
-*/
+  |--------------------------------------------------------------------------
+  | Application Routes
+  |--------------------------------------------------------------------------
+  |
+  | Here is where you can register all of the routes for an application.
+  | It's a breeze. Simply tell Laravel the URIs it should respond to
+  | and give it the Closure to execute when that URI is requested.
+  |
+ */
 
-Route::get('/', 'PagesController@home');
-Route::get('/about', 'PagesController@about');
-//Route::get('/', 
-//        function()
-//{
-//	return View::make('hello');
-//}
-
-//);
+//Route::get('/', 'PagesController@home');
+//Route::get('/about', 'PagesController@about');
+Route::get('/', function() {
+//    $users = DB::table('users')->find(1);
+//    dd($users);
+//    $users = User::all();
+    $user = new User;
+    
+    $user->username = 'Mehedi';
+    $user->password = Hash::make('123456');
+    $user->save();
+    
+    return User::all();
+//    return View::make('hello');
+}
+);
